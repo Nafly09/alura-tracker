36161df99bfe417021fd0acc25b84ac7668a0fef
nafly09 <juniorinterlyte95@hotmail.com>
2022-04-29 15:53:03 -0300

    feat: refactored button to a component

---
+++ src/components/Botão.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="button" @click="clicado" :disabled="desabilitado">
+    <span class="icon">
+      <i :class="icon"></i>
+    </span>
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Button",
+  emits: ["clicado"],
+  props: {
+    desabilitado: {
+      type: Boolean,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    clicado(): void {
+      this.$emit("clicado");
+    },
+  },
+});
+</script>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,53 +1,55 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-    <button class="button" @click="iniciar" :disabled="cronometroRodando">
-      <span class="icon">
-        <i class="fas fa-play"></i>
-      </span>
-      <span>play</span>
-    </button>
-    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-      <span class="icon">
-        <i class="fas fa-stop"></i>
-      </span>
-      <span>stop</span>
-    </button>
+    <Button
+      @clicado="iniciar"
+      icon="fas fa-play"
+      text="Start"
+      :desabilitado="cronometroRodando"
+    />
+    <Button
+      @clicado="finalizar"
+      icon="fas fa-stop"
+      text="Stop"
+      :desabilitado="!cronometroRodando"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Cronometro from './Cronometro.vue'
+import Cronometro from "./Cronometro.vue";
+import Button from "./Botão.vue";
 
 export default defineComponent({
   name: "Temporizador",
-  emits: ['aoTemporizadorFinalizado'],
+  emits: ["aoTemporizadorFinalizado"],
   components: {
-    Cronometro
+    Cronometro,
+    Button,
   },
-  data () {
+  data() {
     return {
       tempoEmSegundos: 0,
       cronometro: 0,
-      cronometroRodando: false
-    }
+      cronometroRodando: false,
+    };
   },
   methods: {
-    iniciar () {
+    iniciar() {
       // começar a contagem
       // 1 seg = 1000 ms
-      this.cronometroRodando = true
+      this.cronometroRodando = true;
       this.cronometro = setInterval(() => {
-        this.tempoEmSegundos += 1        
-      }, 1000)
+        this.tempoEmSegundos += 1;
+      }, 1000);
     },
-    finalizar () {
-      this.cronometroRodando = false
-      clearInterval(this.cronometro)
-      this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos)
-      this.tempoEmSegundos = 0
-    }
-  }
+    finalizar() {
+      this.cronometroRodando = false;
+      clearInterval(this.cronometro);
+      this.$emit("aoTemporizadorFinalizado", this.tempoEmSegundos);
+      this.tempoEmSegundos = 0;
+    },
+  },
 });
 </script>
